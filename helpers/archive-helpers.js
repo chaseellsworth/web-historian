61226dbff39462fdcacdf63585e4fb8f07e82acf
chaseme3 <chaseme3@gmail.com>
2014-11-19 19:32:41 -0800

    connected archive-helpers to request-handler

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 
+
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -28,18 +29,17 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(callback){
-  fs.readFile(exports.paths.list, string, function(err, data){
-    if (err){
-      throw err;
-    } else {
-      callback(data);
-    }
-  });
+exports.readListOfUrls = function(){
+ return fs.readFileSync(exports.paths.list, "utf8");
 };
 
-exports.isUrlInList = function(requstedUrl, data){
-  var urlList = data.split(",");
+
+exports.isUrlInList = function(requestedUrl){
+  console.log("1");
+  var list = exports.readListOfUrls();
+  console.log(list);
+  var urlList = list.split(",");
+  console.log(urlList);
   for (var i = 0; i < urlList.length; i++){
     if(urlList[i] === requestedUrl){
       return true;
--- web/request-handler.js
+++ web/request-handler.js
@@ -10,37 +10,37 @@ var statusCode = 200;
 var headers = helpers.headers;
 
 exports.handleRequest = function (request, response) {
-  console.log("Serving request tpe " + request.method + "for url " + request.url);
-  var parsedUrl = request.url.split("/");
+  // console.log("Serving request tpe " + request.method + "for url " + request.url);
+  // var parsedUrl = request.url.split("/");
 
-  if(request.url === '/'){
-    response.writehead(statusCode, headers);
-    // serveAssest(response, url path HTML, function(){} );
-    // serveAssest(response, url path CSS, function(){} );
-    // serveAssest(response, url path JS, function(){} );
-    response.end();
-  }else if(request.method  === 'GET'){
-    response.writehead(statusCode, headers);
-    if(archive.readUrlList(archive.isUrlInList) === false){
-      // add it to the list
-      // download it
-    };
-    // serveAssest(response, url path HTML, function(){} );
-    // serveAssest(response, url path CSS, function(){} );
-    // serveAssest(response, url path JS, function(){} );
-  response.end();
-  }else if(request.method  === 'POST'){
-    statusCode = 201;
-    response.writehead(statusCode, headers);
-  //   //IF IT IS ALREADY ARCHIVED
-  //     //RETURN THE HTML
-  //   //IF NOT, SAY IT'S NOT AND SHOW THE LOADING HTML PAGE
-  //   //HAVE THE HELPER FUNCTION DO A POST REQUEST TO ARCHIVE THE PAGE
-  //   //HAVE BACKGROUND SYSTEM ARCHIVE IT
-  }else {
-    statusCode = 404;
-    response.writehead(statusCode, headers);
-    response.end(archive.paths.list);
+  // if(request.url === '/'){
+  //   response.writehead(statusCode, headers);
+  //   // serveAssest(response, url path HTML, function(){} );
+  //   // serveAssest(response, url path CSS, function(){} );
+  //   // serveAssest(response, url path JS, function(){} );
+  //   response.end();
+  // }else if(request.method  === 'GET'){
+  //   response.writehead(statusCode, headers);
+  //   if( === false){
+  //     // add it to the list
+  //     // download it
+  //   };
+  //   // serveAssest(response, url path HTML, function(){} );
+  //   // serveAssest(response, url path CSS, function(){} );
+  //   // serveAssest(response, url path JS, function(){} );
+  // response.end();
+  // }else if(request.method  === 'POST'){
+  //   statusCode = 201;
+  //   response.writehead(statusCode, headers);
+  // //   //IF IT IS ALREADY ARCHIVED
+  // //     //RETURN THE HTML
+  // //   //IF NOT, SAY IT'S NOT AND SHOW THE LOADING HTML PAGE
+  // //   //HAVE THE HELPER FUNCTION DO A POST REQUEST TO ARCHIVE THE PAGE
+  // //   //HAVE BACKGROUND SYSTEM ARCHIVE IT
+  // }else {
+  //   statusCode = 404;
+  //   response.writehead(statusCode, headers);
+  //   response.end(archive.paths.list);
 };
-
-exports.handleRequest();
+console.log(archive.isUrlInList("www.google.com"));
+// exports.handleRequest();
